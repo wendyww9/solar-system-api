e1925d1498bea52efc52a3175fd991321bd44400
wendyww9 <wendyskystar@gmail.com>
2025-04-18 11:26:47 -0700

    finish wave2 to add get information of one planet by id

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
@@ -18,3 +18,27 @@ def get_all_planets():
         )
 
     return planets_responce
+
+@planets_bp.get("/<id>")
+def get_one_planet(id):
+    planet = validate_planet(id)
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "species": planet.species
+    }
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        invalid_msg = {"message": f"planet id ({id}) invalid"}
+        abort(make_response(invalid_msg, 400))
+
+    for planet in planets:
+        if planet.id == id:
+            return planet
+    
+    response = {"message": f"planet {id} not found"}
+    abort(make_response(response, 404))

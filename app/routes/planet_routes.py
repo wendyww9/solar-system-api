e4ad06faa6db214adae3e91c9be8c5a6b00911c9
KateM <ekaterinamarantidi@gmail.com>
2025-04-18 12:06:17 -0700

    added `get_one_planet(id)`, wave 2 done

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
@@ -18,3 +18,27 @@ def get_all_planets():
         )
 
     return planets_response
+
+@planets_bp.get("/<id>")
+def get_one_planet(id):
+    planet = validate_planet(id)
+
+    return dict(
+        id = planet.id,
+        name = planet.name,
+        description = planet.description,
+        habitable = planet.habitable
+    )
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        invalid = {"message": f"Planet id {id} is invalid"}
+        abort(make_response(invalid, 400))
+
+    for planet in planets:
+        if planet.id == id:
+            return planet
+    not_found = {"message": f"Planet with id {id} not found"}
+    abort(make_response(not_found, 404))

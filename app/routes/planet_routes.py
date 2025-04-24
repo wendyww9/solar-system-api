ef74ca6224c1952b0d49a4d784c84b7b5c2fc248
KateM <ekaterinamarantidi@gmail.com>
2025-04-24 14:28:10 -0700

    Created POST and GET requests to endpoint '/planets'

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,7 +1,45 @@
-from flask import Blueprint, abort, make_response
+from flask import Blueprint, abort, make_response, request
+from app.models.planet import Planet
+from ..db import db
 # from app.models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    habitable = request_body["habitable"]
+
+    new_planet = Planet(name=name, description=description, habitable=habitable)
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "habitable": new_planet.habitable
+    }
+    return response, 201
+
+@planets_bp.get("")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+    planets_response = []
+    for planet in planets:
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "habitable": planet.habitable
+            }
+        )
+    
+    return planets_response
 
 # @planets_bp.get("")
 # def get_all_planets():

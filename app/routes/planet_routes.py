abb86b6f26c34f7869a71871763dc266771f7bf6
wendyww9 <wendyskystar@gmail.com>
2025-04-24 12:34:48 -0700

    comment out planet_routes

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -17,28 +17,28 @@ planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 #             )
 #         )
 
-    return planets_responce
+#     return planets_responce
 
-@planets_bp.get("/<id>")
-def get_one_planet(id):
-    planet = validate_planet(id)
-    return {
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description,
-        "species": planet.species
-    }
+# @planets_bp.get("/<id>")
+# def get_one_planet(id):
+#     planet = validate_planet(id)
+#     return {
+#         "id": planet.id,
+#         "name": planet.name,
+#         "description": planet.description,
+#         "species": planet.species
+#     }
 
-def validate_planet(id):
-    try:
-        id = int(id)
-    except:
-        invalid_msg = {"message": f"planet id ({id}) invalid"}
-        abort(make_response(invalid_msg, 400))
+# def validate_planet(id):
+#     try:
+#         id = int(id)
+#     except:
+#         invalid_msg = {"message": f"planet id ({id}) invalid"}
+#         abort(make_response(invalid_msg, 400))
 
-    for planet in planets:
-        if planet.id == id:
-            return planet
+#     for planet in planets:
+#         if planet.id == id:
+#             return planet
     
-    response = {"message": f"planet {id} not found"}
-    abort(make_response(response, 404))
+#     response = {"message": f"planet {id} not found"}
+#     abort(make_response(response, 404))

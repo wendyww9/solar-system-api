0d78dc4467d3ff082dd0bd4f13045ef6a3fb3f44
wendyww9 <wendyskystar@gmail.com>
2025-04-30 09:23:05 -0700

    add .env

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,19 @@ from flask import Flask
 from .db import db, migrate
 from .models import planet   
 from .routes.planet_routes import planets_bp
+import os
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:@localhost:5432/solar_system_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
+    if config:
+        # Merge `config` into the app's configuration
+        # to override the app's default settings
+        app.config.update(config)
+        
     db.init_app(app)
     migrate.init_app(app, db)
 

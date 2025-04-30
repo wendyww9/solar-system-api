c697bde5da8600a624dbbe69f9270566aacdbe71
KateM <ekaterinamarantidi@gmail.com>
2025-04-30 01:00:06 -0700

    Created test environment, added tests `test_get_one_planet()`, `test_create_one_planet()`, `test_get_all_planets()`

---
+++ tests/test_planet_routes.py
@@ -0,0 +1,42 @@
+import pytest
+
+# def test_get_all_planets_with_no_records(client):
+    # # Act
+    # response = client.get("/planets")
+    # response_body = response.get_json()
+
+    # # Assert
+    # assert response.status_code == 200
+    # assert response_body == []
+
+def test_get_one_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Take-a-Nap",
+        "description": "Do It Tomorrow",
+        "habitable": 'true'
+    }
+
+def test_create_one_planet(client):
+    # Act
+    response = client.post("/planets", json={
+        "name": "Very New Planet",
+        "description": "Very-very new planet",
+        "habitable": 'false'
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 1,
+        "name": "Very New Planet",
+        "description": "Very-very new planet",
+        "habitable": 'false'
+    }

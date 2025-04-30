37a7a5b763b54418afccc7d4a21be4f622a78e6c
wendyww9 <wendyskystar@gmail.com>
2025-04-30 11:36:47 -0700

    add test function for empty data

--- tests/test_planet_routes.py
+++ tests/test_planet_routes.py
@@ -1,13 +1,13 @@
 import pytest
 
-# def test_get_all_planets_with_no_records(client):
-    # # Act
-    # response = client.get("/planets")
-    # response_body = response.get_json()
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
 
-    # # Assert
-    # assert response.status_code == 200
-    # assert response_body == []
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
 
 def test_get_one_planet(client, two_saved_planets):
     # Act
@@ -21,6 +21,17 @@ def test_get_one_planet(client, two_saved_planets):
         "name": "Take-a-Nap",
         "description": "Do It Tomorrow",
         "habitable": 'true'
+    }
+
+def test_get_one_planet_no_data(client):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {
+        'message': 'planet with 1 does not exist'
     }
 
 def test_create_one_planet(client):
